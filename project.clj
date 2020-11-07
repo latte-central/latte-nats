a3de8a0fce2c4747d1ac7d0bd585f4ee0a2826ad
Frédéric Peschanski <Frederic Peschanski>
2020-11-07 14:34:06 +0100

    version bump

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject latte-nats "0.1.0-SNAPSHOT"
+(defproject latte-nats "0.1.1-SNAPSHOT"
   :description "A formalization of natural numbers in LaTTe."
   :url "https://github.com/latte-central/latte-nats.git"
   :license {:name "MIT Licence"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [latte-sets "1.0b8-SNAPSHOT"]]
+                 [latte-sets "1.0b9-SNAPSHOT"]]
   :main latte-integers.main
   :aliases {"certify" ["run" ":certify"]
             "clear-cert" ["run" ":clear-cert"]}
