1855b965578d32ec6570634d537d878e4c42c89a
Frederic Peschanski <frederic.peschanski@lip6.fr>
2022-12-09 14:46:14 +0100

    algebraic properties of addition

--- src/latte_nats/plus.clj
+++ src/latte_nats/plus.clj
@@ -152,174 +152,172 @@
 
 
 (defthm plus-commute
-  [[n nat] [m nat]]
-  (= (+ n m)
-     (+ m n)))
+  [[m nat] [n nat]]
+  (= (+ m n)
+     (+ n m)))
 
 (proof 'plus-commute
-  "We proceed by induction on `n`."
-  (pose P := (lambda [k nat] (= (+ k m) (+ m k))))
+  "We proceed by induction on `m`."
+  (pose P := (lambda [k nat] (= (+ k n) (+ n k))))
   "First let's prove `(P zero)`."
-  (have <a1> (= m (+ m zero))
-        :by (eq/eq-sym (plus-zero m)))
-  (have <a> (P zero) :by (eq/eq-trans (plus-zero-swap m) <a1>))
+  (have <a1> (= n (+ n zero))
+        :by (eq/eq-sym (plus-zero n)))
+  (have <a> (P zero) :by (eq/eq-trans (plus-zero-swap n) <a1>))
   "Now the inductive cases."
   (assume [k nat
            Hind (P k)]
     "We show `(P (succ k))`."
-    (have <b1> (= (+ (succ k) m)
-                  (succ (+ k m)))
-          :by (plus-succ-swap k m))
-    (have <b2> (= (succ (+ k m))
-                  (succ (+ m k)))
+    (have <b1> (= (+ (succ k) n)
+                  (succ (+ k n)))
+          :by (plus-succ-swap k n))
+    (have <b2> (= (succ (+ k n))
+                  (succ (+ n k)))
           :by (eq/eq-cong succ Hind))
-    (have <b3> (= (+ (succ k) m)
-                  (succ (+ m k))) :by (eq/eq-trans <b1> <b2>))
-    (have <b4> (= (succ (+ m k))
-                  (+ m (succ k))) :by (eq/eq-sym (plus-succ m k)))
+    (have <b3> (= (+ (succ k) n)
+                  (succ (+ n k))) :by (eq/eq-trans <b1> <b2>))
+    (have <b4> (= (succ (+ n k))
+                  (+ n (succ k))) :by (eq/eq-sym (plus-succ n k)))
     (have <b> (P (succ k)) :by (eq/eq-trans <b3> <b4>)))
 
   "Now we apply integer induction."
-  (have <e> (P n) :by ((nats/nat-induct P) <a> <b> n))
+  (have <e> (P m) :by ((nats/nat-induct P) <a> <b> m))
   (qed <e>))
 
-
 (defthm plus-assoc
-  [[n nat] [m nat] [p nat]]
-  (= (+ n (+ m p))
-     (+ (+ n m) p)))
+  [[m nat] [n nat] [p nat]]
+  (= (+ m (+ n p))
+     (+ (+ m n) p)))
 
 (proof 'plus-assoc
     (pose P := (lambda [k nat]
-               (= (+ n (+ m k))
-                  (+ (+ n m) k))))
+               (= (+ m (+ n k))
+                  (+ (+ m n) k))))
   "We prove `P` by induction on `k`."
   "First `(P zero)`"
-  (have <a1> (= (+ n (+ m zero))
-                (+ n m))
-        :by (eq/eq-cong (lambda [k nat] (+ n k))
-                         (plus-zero m)))
-  (have <a2> (= (+ n m)
-                (+ (+ n m) zero))
-        :by (eq/eq-sym (plus-zero (+ n m))))
+  (have <a1> (= (+ m (+ n zero))
+                (+ m n))
+        :by (eq/eq-cong (lambda [k nat] (+ m k))
+                         (plus-zero n)))
+  (have <a2> (= (+ m n)
+                (+ (+ m n) zero))
+        :by (eq/eq-sym (plus-zero (+ m n))))
   (have <a> (P zero) :by (eq/eq-trans <a1> <a2>))
   "Then the inductive cases."
   (assume [p nat
-           Hind (= (+ n (+ m p))
-                   (+ (+ n m) p))]
+           Hind (= (+ m (+ n p))
+                   (+ (+ m n) p))]
     "Let's prove `(P (succ p))`."
-    (have <b1> (= (+ n (+ m (succ p)))
-                  (+ n (succ (+ m p))))
-          :by (eq/eq-cong (lambda [k nat] (+ n k))
-                          (plus-succ m p)))
-    (have <b2>  (= (+ n (succ (+ m p)))
-                   (succ (+ n (+ m p))))
-          :by (plus-succ n (+ m p)))
-    (have <b3> (= (+ n (+ m (succ p)))
-                  (succ (+ n (+ m p))))
+    (have <b1> (= (+ m (+ n (succ p)))
+                  (+ m (succ (+ n p))))
+          :by (eq/eq-cong (lambda [k nat] (+ m k))
+                          (plus-succ n p)))
+    (have <b2>  (= (+ m (succ (+ n p)))
+                   (succ (+ m (+ n p))))
+          :by (plus-succ m (+ n p)))
+    (have <b3> (= (+ m (+ n (succ p)))
+                  (succ (+ m (+ n p))))
           :by (eq/eq-trans <b1> <b2>))
-    (have <b4> (= (succ (+ n (+ m p)))
-                  (succ (+ (+ n m) p)))
+    (have <b4> (= (succ (+ m (+ n p)))
+                  (succ (+ (+ m n) p)))
           :by (eq/eq-cong succ Hind))
-    (have <b5> (= (+ n (+ m (succ p)))
-                  (succ (+ (+ n m) p)))
+    (have <b5> (= (+ m (+ n (succ p)))
+                  (succ (+ (+ m n) p)))
           :by (eq/eq-trans <b3> <b4>))
-    ;; = (+ (+ n m) (succ p))
-    (have <b6> (= (succ (+ (+ n m) p))
-                  (+ (+ n m) (succ p)))
-          :by (eq/eq-sym (plus-succ (+ n m) p)))
+    ;; = (+ (+ m n) (succ p))
+    (have <b6> (= (succ (+ (+ m n) p))
+                  (+ (+ m n) (succ p)))
+          :by (eq/eq-sym (plus-succ (+ m n) p)))
     (have <b> (P (succ p))
           :by (eq/eq-trans <b5> <b6>)))
   (have <c> (P p) :by ((nats/nat-induct P) <a> <b> p))
   (qed <c>))
 
 (defthm plus-comm-assoc
-  [[n nat] [m nat] [p nat]]
-  (= (+ (+ n m) p)
-     (+ (+ n p) m)))
+  [[m nat] [n nat] [p nat]]
+  (= (+ (+ m n) p)
+     (+ (+ m p) n)))
 
 (proof 'plus-comm-assoc
-  (have <a> (= (+ (+ n m) p)
-               (+ n (+ m p)))
-        :by (eq/eq-sym (plus-assoc n m p)))
-  (have <b> (= (+ n (+ m p))
-               (+ n (+ p m)))
+  (have <a> (= (+ (+ m n) p)
+               (+ m (+ n p)))
+        :by (eq/eq-sym (plus-assoc m n p)))
+  (have <b> (= (+ m (+ n p))
+               (+ m (+ p n)))
         :by (eq/eq-cong (lambda [$ nat]
-                          (+ n $)) (plus-commute m p)))
-  (have <c> (= (+ n (+ p m))
-               (+ (+ n p) m))
-        :by (plus-assoc n p m))
-
+                          (+ m $)) (plus-commute n p)))
+  (have <c> (= (+ m (+ p n))
+               (+ (+ m p) n))
+        :by (plus-assoc m p n))
   (qed (eq/eq-trans* <a> <b> <c>)))
 
 
 (defthm plus-right-cancel
-  [[n nat] [m nat] [p nat]]
-  (==> (= (+ n p) (+ m p))
-       (= n m)))
+  [[m nat] [n nat] [p nat]]
+  (==> (= (+ m p) (+ n p))
+       (= m n)))
 
 (proof 'plus-right-cancel
   "We proceed by induction."
   (pose P := (lambda [k nat]
-               (==> (= (+ n k) (+ m k))
-                    (= n m))))
+               (==> (= (+ m k) (+ n k))
+                    (= m n))))
   "Base case."
-  (assume [Hz (= (+ n zero) (+ m zero))]
-    (have <a1> (= n (+ m zero))
-          :by (eq/rewrite Hz (plus-zero n)))
-    (have <a2> (= n m)
-          :by (eq/rewrite <a1> (plus-zero m))))
+  (assume [Hz (= (+ m zero) (+ n zero))]
+    (have <a1> (= m (+ n zero))
+          :by (eq/rewrite Hz (plus-zero m)))
+    (have <a2> (= m n)
+          :by (eq/rewrite <a1> (plus-zero n))))
   (have <a> (P zero) :by <a2>)
   "Inductive case."
   (assume [k nat
            Hk (P k)]
     "Successor case."
-    (assume [Hsucc (= (+ n (succ k)) (+ m (succ k)))]
-      (have <b1> (= (succ (+ n k)) (+ m (succ k)))
-            :by (eq/rewrite Hsucc (plus-succ n k)))
-      (have <b2> (= (succ (+ n k)) (succ (+ m k)))
-            :by (eq/rewrite <b1> (plus-succ m k)))
-      (have <b3> (= (+ n k) (+ m k)) :by (nats/succ-injective (+ n k) (+ m k) <b2>))
-      (have <b4> (= n m) :by (Hk <b3>)))
+    (assume [Hsucc (= (+ m (succ k)) (+ n (succ k)))]
+      (have <b1> (= (succ (+ m k)) (+ n (succ k)))
+            :by (eq/rewrite Hsucc (plus-succ m k)))
+      (have <b2> (= (succ (+ m k)) (succ (+ n k)))
+            :by (eq/rewrite <b1> (plus-succ n k)))
+      (have <b3> (= (+ m k) (+ n k)) :by (nats/succ-injective (+ m k) (+ n k) <b2>))
+      (have <b4> (= m n) :by (Hk <b3>)))
     (have <b> (P (succ k)) :by <b4>))
   "We apply the induction principle."
   (have <c> (P p) :by ((nats/nat-induct P) <a> <b> p))
   (qed <c>))
 
 (defthm plus-left-cancel
-  [[n nat] [m nat] [p nat]]
-  (==>  (= (+ p n) (+ p m))
-        (= n m)))
+  [[m nat] [n nat] [p nat]]
+  (==>  (= (+ m n) (+ m p))
+        (= n p)))
 
 (proof 'plus-left-cancel
-  (assume [H (= (+ p n) (+ p m))]
-    (have <a> (= (+ n p) (+ p m))
-          :by (eq/rewrite H (plus-commute p n)))
-    (have <b> (= (+ n p) (+ m p))
-          :by (eq/rewrite <a> (plus-commute p m)))
-    (have <c> (= n m) :by ((plus-right-cancel n m p) <b>)))
+  (assume [H (= (+ m n) (+ m p))]
+    (have <a> (= (+ n m) (+ m p))
+          :by (eq/rewrite H (plus-commute m n))) 
+    (have <b> (= (+ n m) (+ p m))
+          :by (eq/rewrite <a> (plus-commute m p)))
+    (have <c> (= n p) :by ((plus-right-cancel n p m) <b>)))
   (qed <c>))
 
 (defthm plus-right-cancel-conv
-  [[n nat] [m nat] [p nat]]
-  (==> (= n m)
-       (= (+ n p) (+ m p))))
+  [[m nat] [n nat] [p nat]]
+  (==> (= m n)
+       (= (+ m p) (+ n p))))
 
 (proof 'plus-right-cancel-conv
-  (assume [H (= n m)]
-    (have <a> (= (+ n p) (+ m p))
+  (assume [H (= m n)]
+    (have <a> (= (+ m p) (+ n p))
           :by (eq/eq-cong (lambda [k nat] (+ k p))
                           H)))
   (qed <a>))
 
 (defthm plus-left-cancel-conv
-  [[n nat] [m nat] [p nat]]
-  (==> (= n m)
-       (= (+ p n) (+ p m))))
+  [[m nat] [n nat] [p nat]]
+  (==> (= m n)
+       (= (+ p m) (+ p n))))
 
 (proof 'plus-left-cancel-conv
-  (assume [H (= n m)]
-    (have <a> (= (+ p n) (+ p m))
+  (assume [H (= m n)]
+    (have <a> (= (+ p m) (+ p n))
           :by (eq/eq-cong (lambda [k nat] (+ p k))
                           H)))
   (qed <a>))
@@ -336,7 +334,7 @@
     (have <b> (= (+ n k) (+ n zero))
           :by (eq/nrewrite 2 H <a>))
     (have <c> (= k zero)
-          :by ((plus-left-cancel k zero n) <b>)))
+          :by ((plus-left-cancel n k zero) <b>)))
   (qed <c>))
 
 
@@ -383,8 +381,9 @@
   (qed (eq/rewrite <a> <b>)))
 
 
-;; Algebraic properties
-
+;; ================================
+;; Algebraic properties of addition
+;; ================================
 
 (defthm associative-plus
   []
@@ -429,3 +428,41 @@
   (have <b> (q/single P) :by (alg/identity-single +))
   (qed (p/and-intro <a> <b>)))
 
+(defthm cancel-left-plus
+  []
+  (alg/cancel-left +))
+
+(proof 'cancel-left-plus
+  (qed plus-left-cancel))
+
+(defthm cancel-right-plus
+  []
+  (alg/cancel-right +))
+
+(proof 'cancel-right-plus
+  (assume [x nat y nat z nat]
+    (have <a> _ :by (plus-right-cancel y z x)))
+  (qed <a>))
+
+(defthm monoid-plus
+  []
+  (alg/monoid + zero))
+
+(proof 'monoid-plus
+  (qed (p/and-intro* associative-plus
+                     identity-left-plus
+                     identity-right-plus)))
+
+(defthm commutative-plus
+  []
+  (alg/commutative +))
+
+(proof 'commutative-plus
+  (qed plus-commute))
+
+
+
+
+
+  
+    
